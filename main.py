9ecba734890f41c647ea02edb09623bae372a2ff
nasoooor <nasoooor29@gmail.com>
2026-05-17 07:09:33 +0300

    orgnize shit

--- main.py
+++ main.py
@@ -4,28 +4,27 @@ from selectolax.parser import HTMLParser
 
 
 def main(input: str, output: str):
-    # unzip archive to output + "unzipped"
-    unzipped_path = Path(output) / "unzipped"
-    with zipfile.ZipFile(input, "r") as zip_ref:
-        zip_ref.extractall(unzipped_path)
+    unzipped_path = extract_zip_archive(input, output)
+    txt_path = process_html_files(output, unzipped_path)
+    # convert_to_epub(output, txt_path)
+
+
+def process_html_files(output, unzipped_path):
     # go over everything on unzipped_path use selectolax to get first h1 as title clean it,
     # then grab all p tags join by \n and save to output + title + .txt
     # save txt to output / txt / title + .txt
-
+    output_txt_path = Path(output) / "txt"
     for file in unzipped_path.glob("**/*.html"):
         with open(file, "r", encoding="utf-8") as f:
             html = f.read()
 
         parser = HTMLParser(html)
-        title = (
-            parser.css_first("h1")
-            .text()
-            .strip()
-            .lower()
-            .replace("chapter chapter", "chapter")
-        )
+        title = parser.css_first("h1").text().strip().lower()
+        # sanitize title by replacing spaces with underscores and removing special characters
+        title = sanitize_title(title)
+        title = title.replace("chapter chapter", "chapter")
         content = "\n".join([p.text().strip() for p in parser.css("p")])
-        output_file = Path(output) / "txt" / f"{title}.txt"
+        output_file = output_txt_path/ f"{title}.txt"
         output_file.parent.mkdir(parents=True, exist_ok=True)
         with open(output_file, "w", encoding="utf-8") as f:
             f.write(title + "\n\n" + content)
@@ -35,6 +34,29 @@ def main(input: str, output: str):
     print(
         f"total files on output/txt: {len(list((Path(output) / 'txt').glob('**/*.txt')))}"
     )
+    return output_txt_path
+
+
+def sanitize_title(title: str):
+    return (
+        title.replace("/", "_")
+        .replace("\\", "_")
+        .replace(":", "_")
+        .replace("*", "_")
+        .replace("?", "_")
+        .replace('"', "_")
+        .replace("<", "_")
+        .replace(">", "_")
+        .replace("|", "_")
+    )
+
+
+def extract_zip_archive(input, output):
+    # unzip archive to output + "unzipped"
+    unzipped_path = Path(output) / "unzipped"
+    with zipfile.ZipFile(input, "r") as zip_ref:
+        zip_ref.extractall(unzipped_path)
+    return unzipped_path
 
 
 if __name__ == "__main__":
